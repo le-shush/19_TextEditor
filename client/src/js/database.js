ebf15c986e38959693635bdd2a449c28c49d3b52
LeShush <mycelium.architecture@gmail.com>
2023-08-15 15:26:59 -0600

    database config

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,26 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('PUT to db', content);
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = await store.put({id: id, jate: content});
+  const result = await request.result;
+  console.log('PUT result', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET from db');
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = await store.getAll();
+  const result = await request.result;
+  console.log('GET result', result);
+  return result;
+}
 
 initdb();
